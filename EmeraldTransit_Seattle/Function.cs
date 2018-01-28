ab769a2e115de24265b422a7eb970d3ab3c719b8
Kasey Uhlenhuth <kaseyu@microsoft.com>
2018-01-27 22:27:26 -0800

    MVP with RouteName slots

--- EmeraldTransit_Seattle/Function.cs
+++ EmeraldTransit_Seattle/Function.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -12,16 +15,86 @@ namespace EmeraldTransit_Seattle
 {
     public class Function
     {
-        
+
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            //create a response to return
+            SkillResponse response = new SkillResponse();
+            response.Response = new ResponseBody();
+            response.Response.ShouldEndSession = false;
+            IOutputSpeech innerResponse = new PlainTextOutputSpeech() { Text = "testing" };
+            var log = context.Logger;
+
+
+            Type requestType = input.GetRequestType();
+            if (requestType == typeof(LaunchRequest))
+            {
+                log.LogLine($"Default LaunchRequest made");
+                innerResponse = new PlainTextOutputSpeech();
+                (innerResponse as PlainTextOutputSpeech).Text = "Where would you like to go?";
+            }
+            else if (requestType == typeof(IntentRequest))
+            {
+                var intentRequest = (IntentRequest)input.Request;
+                switch (intentRequest.Intent.Name)
+                {
+                    case "AMAZON.CancelIntent":
+                        log.LogLine($"AMAZON.CancelIntent: send StopMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "Stopping!";
+                        response.Response.ShouldEndSession = true;
+                        break;
+                    case "AMAZON.StopIntent":
+                        log.LogLine($"AMAZON.StopIntent: send StopMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "Stopping";
+                        response.Response.ShouldEndSession = true;
+                        break;
+                    case "AMAZON.HelpIntent":
+                        log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "Say the name of your bus route.";
+                        break;
+                    case "GetRouteIntent":
+                        log.LogLine($"Intent Requested {intentRequest.Intent.Name}");
+                        innerResponse = new PlainTextOutputSpeech();
+                        string value = intentRequest.Intent.Slots["RouteName"].Value;
+                        (innerResponse as PlainTextOutputSpeech).Text = GetRouteName(value);
+                        break;
+                    default:
+                        log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "Say the name of your bus route";
+                        break;
+                }
+            }
+
+            response.Response.OutputSpeech = innerResponse;
+            response.Version = "1.0";
+            return response;
         }
+        public string GetRouteName(string route) => route;
+    }
+
+    public class BusInfoResource
+    {
+        public string Language { get; set; }
+        public string SkillName { get; set; }
+        public string HelpMessage { get; set; }
+        public string HelpReprompt { get; set; }
+        public string StopMessage { get; set; }
+
+        public BusInfoResource(string language)
+        {
+            Language = language ?? throw new ArgumentNullException(nameof(language));
+        }
+
+
     }
 }
