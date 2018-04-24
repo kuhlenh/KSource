3d085c740909a09924ec32ee3e666feccad451bb
Kasey Uhlenhuth <kaseyu@microsoft.com>
2018-04-24 13:20:47 -0700

    it works!

--- EmeraldTransit_Seattle/Function.cs
+++ EmeraldTransit_Seattle/Function.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Alexa.NET;
 using Alexa.NET.Request;
@@ -21,8 +22,10 @@ using Newtonsoft.Json.Linq;
 
 namespace EmeraldTransit_Seattle
 {
+
     public class Function
     {
+        HttpClient client = new HttpClient();
         //Func<T> GetResult<T>(T prototype) { return () => prototype; }
 
         /// <summary>
@@ -33,6 +36,7 @@ namespace EmeraldTransit_Seattle
         /// <returns></returns>
         public async Task<SkillResponse> FunctionHandler(SkillRequest input, ILambdaContext context)
         {
+
             //create a response to return
             SkillResponse response = new SkillResponse();
             response.Response = new ResponseBody();
@@ -40,10 +44,12 @@ namespace EmeraldTransit_Seattle
             IOutputSpeech innerResponse = new PlainTextOutputSpeech() { Text = "testing" };
             var log = context.Logger;
 
+            log.LogLine("\nStarting function handler...");
+
             Type requestType = input.GetRequestType();
             if (requestType == typeof(LaunchRequest))
             {
-                log.LogLine($"Default LaunchRequest made");
+                log.LogLine($"\nDefault LaunchRequest made");
                 innerResponse = new PlainTextOutputSpeech();
                 (innerResponse as PlainTextOutputSpeech).Text = "Where would you like to go?";
             }
@@ -53,53 +59,97 @@ namespace EmeraldTransit_Seattle
                 switch (intentRequest.Intent.Name)
                 {
                     case "AMAZON.CancelIntent":
-                        log.LogLine($"AMAZON.CancelIntent: send StopMessage");
+                        log.LogLine($"\nAMAZON.CancelIntent: send StopMessage");
                         innerResponse = new PlainTextOutputSpeech();
                         (innerResponse as PlainTextOutputSpeech).Text = "Stopping!";
                         response.Response.ShouldEndSession = true;
                         break;
                     case "AMAZON.StopIntent":
-                        log.LogLine($"AMAZON.StopIntent: send StopMessage");
+                        log.LogLine($"\nAMAZON.StopIntent: send StopMessage");
                         innerResponse = new PlainTextOutputSpeech();
                         (innerResponse as PlainTextOutputSpeech).Text = "Stopping";
                         response.Response.ShouldEndSession = true;
                         break;
                     case "AMAZON.HelpIntent":
-                        log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
+                        log.LogLine($"\nAMAZON.HelpIntent: send HelpMessage");
                         innerResponse = new PlainTextOutputSpeech();
                         (innerResponse as PlainTextOutputSpeech).Text = "Say the name of your bus route.";
                         break;
                     case "GetRouteIntent":
-                        log.LogLine($"Intent Requested {intentRequest.Intent.Name}");
-                        innerResponse = new PlainTextOutputSpeech();
-                        string value = intentRequest.Intent.Slots["RouteName"].Value;
-                        var location = await GetLatLonForUserLocation(input.Context.System, log);
-                        if (location.Item1=="")
+                        log.LogLine("\nStarting GetRouteIntent..." + "\n" + intentRequest.ToString());
+
+                        var consentToken = input.Context.System.ApiAccessToken;
+                        log.LogLine("\nconsent token: " + consentToken);
+
+                        if (string.IsNullOrEmpty(consentToken))
                         {
-                            var resp = new AskForPermissionsConsentCard();
-                            resp.Permissions.Add(RequestedPermission.FullAddress);
+                            log.LogLine("\nUser did not give permission to access their location.");
+                            var permissionCard = new AskForPermissionsConsentCard();
+                            permissionCard.Permissions.Add(RequestedPermission.FullAddress);
                             var speech = new SsmlOutputSpeech();
                             speech.Ssml = "<speak>You need to enable permissions.</speak>";
 
-                            // create the card response
-                            var cardResponse = ResponseBuilder.TellWithAskForPermissionsConsentCard(speech, resp.Permissions);
-                            return cardResponse;
+                            // create and return the card response
+                            response = ResponseBuilder.TellWithAskForPermissionsConsentCard(speech, permissionCard.Permissions);
+                            return response;
                         }
-                        var (lat, lon) = ((location.Item1.Length != 0) && (location.Item2.Length != 0)) ? location : ("40.611959", "-120.332893");
-                        //("47.611959", "-122.332893")
-                        MyStopInfo busInfo = new MyStopInfo(new BusLocator(), new TimeZoneConverter());
-                        var arrivalTimes = await busInfo.GetArrivalTimesForRouteName(value, lat, lon);
-                        StringBuilder sb = new StringBuilder();
-                        sb.Append($"The next {value} comes in ");
-                        foreach (var arrival in arrivalTimes)
-                        {
-                            sb.Append(arrival + " minutes,");
-                        }
-                        (innerResponse as PlainTextOutputSpeech).Text = sb.ToString();
-                        log.LogLine($"route:{value}, lat: {lat}, lon: {lon}");
+
+                        //test linq
+                        List<int> c = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+                        var r = from i in c select i + 1;
+
+                        List<int> c1 = new List<int> { 1, 2, 3, 4, 5, 7 };
+                        List<int> c2 = new List<int> { 10, 30, 40, 50, 60, 70 };
+                        var r2 = from x1 in c1
+                                   join x2 in c2 on x1 equals x2 / 10
+                                   select x1 + x2;
+
+                        // ^^^^ test linq
+
+                        var deviceId = input.Context.System.Device.DeviceID;
+                        var apiEndpoint = input.Context.System.ApiEndpoint;
+
+                        //log.LogLine("\nTrying old version...");
+                        //var geocode = GetLatLonForUserLocation(input.Context.System, log);
+                        //log.LogLine("\nGeocode:" + geocode.ToString());
+                        log.LogLine("\nTrying new version...");
+                        var alexaDeviceAddressClient = new AlexaDeviceAddressClient(apiEndpoint, deviceId, consentToken);
+                        var address = await alexaDeviceAddressClient.GetFullAddressAsync(log);
+                        log.LogLine("==================================");
+                        log.LogLine("\nAddress: " + address);
+                        log.LogLine("==================================");
+
+
+                        //log.LogLine($"Intent Requested {intentRequest.Intent.Name}");
+                        //innerResponse = new PlainTextOutputSpeech();
+                        //string value = intentRequest.Intent.Slots["RouteName"].Value;
+                        //var location = await GetLatLonForUserLocation(input.Context.System, log);
+                        //if (location.Item1=="")
+                        //{
+                        //    var resp = new AskForPermissionsConsentCard();
+                        //    resp.Permissions.Add(RequestedPermission.FullAddress);
+                        //    var speech = new SsmlOutputSpeech();
+                        //    speech.Ssml = "<speak>You need to enable permissions.</speak>";
+
+                        //    // create the card response
+                        //    var cardResponse = ResponseBuilder.TellWithAskForPermissionsConsentCard(speech, resp.Permissions);
+                        //    return cardResponse;
+                        //}
+                        //var (lat, lon) = ((location.Item1.Length != 0) && (location.Item2.Length != 0)) ? location : ("40.611959", "-120.332893");
+                        ////("47.611959", "-122.332893")
+                        //MyStopInfo busInfo = new MyStopInfo(new BusLocator(), new TimeZoneConverter());
+                        //var arrivalTimes = await busInfo.GetArrivalTimesForRouteName(value, lat, lon);
+                        //StringBuilder sb = new StringBuilder();
+                        //sb.Append($"The next {value} comes in ");
+                        //foreach (var arrival in arrivalTimes)
+                        //{
+                        //    sb.Append(arrival + " minutes,");
+                        //}
+                        //(innerResponse as PlainTextOutputSpeech).Text = sb.ToString();
+                        //log.LogLine($"route:{value}, lat: {lat}, lon: {lon}");
                         break;
                     default:
-                        log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
+                        log.LogLine($"\nUnknown intent: " + intentRequest.Intent.Name);
                         innerResponse = new PlainTextOutputSpeech();
                         (innerResponse as PlainTextOutputSpeech).Text = "Say the name of your bus route";
                         break;
@@ -116,12 +166,11 @@ namespace EmeraldTransit_Seattle
             var accessToken = system.ApiAccessToken;
             var deviceId = system.Device.DeviceID;
 
-            HttpClient client = new HttpClient() { };
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
             client.DefaultRequestHeaders.Add("Accept", "application/json");
             var uri = $"https://api.amazonalexa.com/v1/devices/{deviceId}/settings/address";
-            
+
             var response = await client.GetAsync(uri);
 
             log.LogLine("\n request message: " + response.RequestMessage);
@@ -143,6 +192,8 @@ namespace EmeraldTransit_Seattle
                 var json2 = await response2.Content.ReadAsStringAsync();
                 var geocode = JsonConvert.DeserializeObject<Geocode>(json2);
                 log.LogLine("\ngoogle: " + json2);
+                log.LogLine("lat " + geocode.results.FirstOrDefault().geometry.location.lat.ToString());
+                log.LogLine("lng" + geocode.results.FirstOrDefault().geometry.location.lng.ToString());
                 return (geocode.results.FirstOrDefault().geometry.location.lat.ToString(),
                     geocode.results.FirstOrDefault().geometry.location.lng.ToString());
             }
@@ -150,7 +201,7 @@ namespace EmeraldTransit_Seattle
             {
                 throw new Exception(response.StatusCode + " " + response.RequestMessage);
             }
-       
+
             return ("", "");
 
         }
@@ -158,84 +209,149 @@ namespace EmeraldTransit_Seattle
         public string GetRouteName(string route) => route;
     }
 
-
-    public class AddressComponent
+    class AlexaDeviceAddressClient
     {
-        public string long_name { get; set; }
-        public string short_name { get; set; }
-        public List<string> types { get; set; }
-    }
+        public string ApiEndpoint { get; set; }
+        public string DeviceId { get; set; }
+        public string ConsentToken { get; set; }
+        HttpClient client = new HttpClient();
 
-    public class Location
-    {
-        public double lat { get; set; }
-        public double lng { get; set; }
-    }
-
-    public class Northeast
-    {
-        public double lat { get; set; }
-        public double lng { get; set; }
-    }
-
-    public class Southwest
-    {
-        public double lat { get; set; }
-        public double lng { get; set; }
-    }
-
-    public class Viewport
-    {
-        public Northeast northeast { get; set; }
-        public Southwest southwest { get; set; }
-    }
-
-    public class Geometry
-    {
-        public Location location { get; set; }
-        public string location_type { get; set; }
-        public Viewport viewport { get; set; }
-    }
-
-    public class Result
-    {
-        public List<AddressComponent> address_components { get; set; }
-        public string formatted_address { get; set; }
-        public Geometry geometry { get; set; }
-        public string place_id { get; set; }
-        public List<string> types { get; set; }
-    }
-
-    public class Geocode
-    {
-        public List<Result> results { get; set; }
-        public string status { get; set; }
-    }
-    public class BusInfoResource
-    {
-        public string Language { get; set; }
-        public string SkillName { get; set; }
-        public string HelpMessage { get; set; }
-        public string HelpReprompt { get; set; }
-        public string StopMessage { get; set; }
-
-        public BusInfoResource(string language)
+        public AlexaDeviceAddressClient(string apiEndpoint, string deviceId, string consentToken)
         {
-            Language = language ?? throw new ArgumentNullException(nameof(language));
+
+            ApiEndpoint = Regex.Replace(apiEndpoint, @"/^https?:\/\//i", "");
+            DeviceId = deviceId;
+            ConsentToken = consentToken;
+        }
+
+        public async Task<string> GetFullAddressAsync(ILambdaLogger logger)
+        {
+            logger.LogLine("Starting GetFullAddress...");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ConsentToken);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            var requestOptions = $"/v1/devices/{DeviceId}/settings/address/";
+            //var uri = new Uri($"{ApiEndpoint}{requestOptions}");
+            var uri = $"https://api.amazonalexa.com/v1/devices/{DeviceId}/settings/address";
+
+            logger.LogLine($"\n\nURI: {uri}");
+
+            var response = await client.GetAsync(uri);
+
+            var code = response.StatusCode;
+            logger.LogLine($"\nHttpCode for AlexaDeviceAddressClient: {code}");
+
+            //DEMO: convert if to switch
+            if (code == HttpStatusCode.OK)
+            {
+                logger.LogLine("\nAddress successfully retrieved, now responding to user.");
+                var content = await response.Content.ReadAsStringAsync();
+                var json = JsonConvert.DeserializeObject<Address>(content);
+                logger.LogLine("\nAddress is: " + json.ToString());
+                return json.ToString();
+            }
+            else if (code == HttpStatusCode.NoContent)
+            {
+                logger.LogLine("\nSuccessfully requested from the device address API, but no address was returned");
+                return "";
+            }
+            else if (code == HttpStatusCode.Forbidden)
+            {
+                logger.LogLine("\nThe consent token we had wasn't authorized to access the user's address. ");
+                return "";
+            }
+            else
+            {
+                logger.LogLine("\nUnknown location failure.");
+                return "";
+            }
+            // DEMO remove unreachable code
+            logger.LogLine("Ending GetFullAddress...");
         }
 
 
-    }
-    public class Address
+}
+
+public class AddressComponent
+{
+    public string long_name { get; set; }
+    public string short_name { get; set; }
+    public List<string> types { get; set; }
+}
+
+public class Location
+{
+    public double lat { get; set; }
+    public double lng { get; set; }
+}
+
+public class Northeast
+{
+    public double lat { get; set; }
+    public double lng { get; set; }
+}
+
+public class Southwest
+{
+    public double lat { get; set; }
+    public double lng { get; set; }
+}
+
+public class Viewport
+{
+    public Northeast northeast { get; set; }
+    public Southwest southwest { get; set; }
+}
+
+public class Geometry
+{
+    public Location location { get; set; }
+    public string location_type { get; set; }
+    public Viewport viewport { get; set; }
+}
+
+public class Result
+{
+    public List<AddressComponent> address_components { get; set; }
+    public string formatted_address { get; set; }
+    public Geometry geometry { get; set; }
+    public string place_id { get; set; }
+    public List<string> types { get; set; }
+}
+
+public class Geocode
+{
+    public List<Result> results { get; set; }
+    public string status { get; set; }
+}
+public class BusInfoResource
+{
+    public string Language { get; set; }
+    public string SkillName { get; set; }
+    public string HelpMessage { get; set; }
+    public string HelpReprompt { get; set; }
+    public string StopMessage { get; set; }
+
+    public BusInfoResource(string language)
     {
-        public string stateOrRegion { get; set; }
-        public string city { get; set; }
-        public string countryCode { get; set; }
-        public string postalCode { get; set; }
-        public string addressLine1 { get; set; }
-        public string addressLine2 { get; set; }
-        public string addressLine3 { get; set; }
-        public string districtOrCounty { get; set; }
+        Language = language ?? throw new ArgumentNullException(nameof(language));
+    }
+}
+public class Address
+{
+    public string stateOrRegion { get; set; }
+    public string city { get; set; }
+    public string countryCode { get; set; }
+    public string postalCode { get; set; }
+    public string addressLine1 { get; set; }
+    public string addressLine2 { get; set; }
+    public string addressLine3 { get; set; }
+    public string districtOrCounty { get; set; }
+
+        public override string ToString()
+        {
+            return $"{addressLine1},{city},{stateOrRegion},{postalCode}";
+        }
     }
 
 
