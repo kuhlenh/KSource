1287cf2f95899127d2fa1f5f29fc2cefa505a197
Andrew Hall (DEVDIV) <andrehal@microsoft.com>
2018-04-30 22:39:45 -0700

    Adding logic to make unit test sample data relative for both LUT and non-LUT scenarios

--- BusInfo/MockBusLocator.cs
+++ BusInfo/MockBusLocator.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace BusInfo
@@ -10,15 +12,39 @@ namespace BusInfo
             return File.ReadAllText(file);
         }
 
+        private static string GetTestProjectRootFolder()
+        {
+            const string ProjectName = "BusInfoTests";
+            string rootPath;
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            var dir = Path.GetDirectoryName(path);
+
+            //running in the context of LUT, and the path needs to be adjusted
+            if (dir.Contains(".vs"))
+            {
+                rootPath = $"{dir.Substring(0, dir.IndexOf("\\.vs\\") + 1)}{ProjectName}\\";
+            }
+            else
+            {
+                rootPath = dir.Substring(0, dir.IndexOf("\\bin\\") + 1);
+            }
+
+            return rootPath;
+        }
+
         public Task<string> GetJsonForArrivals(string stopId)
         {
-            var json = LoadJson(@"C:\Users\kaseyu\Source\Repos\FunctionApp1\UnitTestProject\Arrivals.json");
+            var rootPath = MockBusLocator.GetTestProjectRootFolder();
+            var json = LoadJson($"{rootPath}Arrivals.json");
             return Task.FromResult(json);
         }
 
         public Task<string> GetJsonForStopsFromLatLongAsync(string lat, string lon)
         {
-            var json = LoadJson(@"C:\Users\kaseyu\Source\Repos\FunctionApp1\UnitTestProject\StopsForLoc.json");
+            var rootPath = MockBusLocator.GetTestProjectRootFolder();
+            var json = LoadJson($"{rootPath}StopsForLoc.json");
             return Task.FromResult(json);
         }
     }
