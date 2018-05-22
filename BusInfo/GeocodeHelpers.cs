9e8bca388726c8609f0dcf2c423933fd40d8c7a1
Kasey Uhlenhuth <kaseyu@microsoft.com>
2018-05-22 10:18:09 -0700

    Cleaning up this file

--- BusInfo/GeocodeHelpers.cs
+++ BusInfo/GeocodeHelpers.cs
@@ -1,33 +1,26 @@
 ﻿using System;
 
-namespace BusInfo
-{
-    public class GeocodeHelpers
-    {
-        public static (string lat, string lon) GetDefaultLocation() 
-        {
+namespace BusInfo {
+    public class GeocodeHelpers {
+        public static (string lat, string lon) GetDefaultLocation() {
             return ("47.639905", "-122.125485");
         }
 
-        public static string PrettyPrintGeocode(string lat, string lon)
-        {
-            double latitude = Double.Parse(lat);
-            var longitude = Double.Parse(lon);
+        public static string PrettyPrintGeocode(string lat, string lon) {
+            var latitude = double.Parse(lat);
+            var longitude = double.Parse(lon);
             return $"\nCurrent location: ({latitude},{longitude})\n";
         }
 
         // Checks if given latitude and longitude are valid entries
-        public static void ValidateLatLon(string lat, string lon)
-        {
-            if (lat.Length > 0 && lon.Length > 0)
-            {
-                double latDouble = double.Parse(lat);
-                double lonDouble = double.Parse(lon);
-                if (!(latDouble >= -90) || !(latDouble <= 90) || !(lonDouble >= -180) || !(lonDouble <= 180))
+        public static void ValidateLatLon(string lat, string lon) {
+            if (lat.Length > 0 && lon.Length > 0) {
+                var latDouble = double.Parse(lat);
+                var lonDouble = double.Parse(lon);
+                if (!(latDouble >= -90) || !(latDouble <= 90) || !(lonDouble >= -180) || !(lonDouble <= 180)) {
                     throw new ArgumentException("Exceeds boundaries. Not a valid latitude or longitude.");
-            }
-            else
-            {
+                }
+            } else {
                 throw new ArgumentException("You are missing latitude and longitude.");
             }
         }
