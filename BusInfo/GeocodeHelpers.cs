cf34c055b7e2a245f52980b3d0581722cf1c38eb
Kasey Uhlenhuth <kaseyu@microsoft.com>
2018-06-01 00:48:03 -0700

    dmeo setup

--- BusInfo/GeocodeHelpers.cs
+++ BusInfo/GeocodeHelpers.cs
@@ -1,33 +1,37 @@
-﻿using System;
+﻿using BusInfo;
+using BusInfoHelpers;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
 
 namespace BusInfo
 {
     public class GeocodeHelpers
     {
-        public static (string lat, string lon) GetDefaultLocation() 
-        {
-            return ("47.639905", "-122.125485");
-        }
+        private static string s_latitude = "47.639905";
+        private static string s_longitude = "-122.125485";
 
+        public static (string lat, string lon) GetDefaultLocation()
+        {
+            return (s_latitude, s_longitude);
+        }
         public static string PrettyPrintGeocode(string lat, string lon)
         {
-            double latitude = Double.Parse(lat);
-            var longitude = Double.Parse(lon);
+            var latitude = double.Parse(lat);
+            var longitude = double.Parse(lon);
             return $"\nCurrent location: ({latitude},{longitude})\n";
         }
 
         // Checks if given latitude and longitude are valid entries
-        public static void ValidateLatLon(string lat, string lon)
-        {
-            if (lat.Length > 0 && lon.Length > 0)
-            {
+        public static void ValidateLatLon(string lat, string lon) {
+            if (lat.Length > 0 && lon.Length > 0) {
                 double latDouble = double.Parse(lat);
-                double lonDouble = double.Parse(lon);
+                var lonDouble = double.Parse(lon);
                 if (!(latDouble >= -90) || !(latDouble <= 90) || !(lonDouble >= -180) || !(lonDouble <= 180))
+                {
                     throw new ArgumentException("Exceeds boundaries. Not a valid latitude or longitude.");
-            }
-            else
-            {
+                }
+            } else {
                 throw new ArgumentException("You are missing latitude and longitude.");
             }
         }
